766a2c01901c0540c988323b65a4652791716b82
staman1702 <>
2023-09-30 16:23:36 +0000

    Add main function and function that returns random choice from our list

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +13,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman')
 
-words = SHEET.worksheet('words')
 
-data = words.get_all_values()
+def get_random_word():
+    """
+    Collects all the words in our sheet and 
+    selects one random word to return.
+    """
+        
+    words = ["canal", "mango" , "fruit", "banana", "brain", "google"]
 
-print(data)
+    # getting random word
+    randomWord = random.choice(words)
+
+    print(randomWord)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    get_random_word()
+    
+main()
